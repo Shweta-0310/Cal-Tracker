4d9458a73e24947b6b885a34faaffa78df30d35e
shwetaxdesign <shwetay.design@gmail.com>
2026-03-18 18:45:20 +0530

    Initial Commit

---
+++ Cal Tracker/Cal_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cal_TrackerApp.swift
+//  Cal Tracker
+//
+//  Created by Shweta Yadav on 18/03/26.
+//
+
+import SwiftUI
+
+@main
+struct Cal_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
